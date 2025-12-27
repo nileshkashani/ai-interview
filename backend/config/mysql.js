451dcc4446be06a8f9622f0eb5a328987f8e42c9
nilesh kashani <nileshkashani45@gmail.com>
2025-12-27 20:48:24 +0530

    Update MySQL configuration for production environment

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -1,9 +1,21 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('interview_app', 'root', 'root', {
-  host: 'localhost',
-  dialect: 'mysql',
-  logging: false
-})
+const sequelize = new Sequelize(
+          'railway',
+          'root',
+          'JSJFRfMpKmIFHGZEtOBCPMtPJQdfmWIR',
+          {
+                      host: 'caboose.proxy.rlwy.net',
+                      port: Number('33504'),
+                      dialect: 'mysql',
+                      logging: false,
+                      dialectOptions: {
+                                    ssl: {
+                                                    require: true,
+                                                    rejectUnauthorized: false
+                                    }
+                      }
+          }
+)
 
 module.exports = sequelize
